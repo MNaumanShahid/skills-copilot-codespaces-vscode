6f1bc281f5bc88bc92a00e0c9e1d41d3132ab9ae
Mohammad Nauman Shahid <133298903+MNaumanShahid@users.noreply.github.com>
2023-08-23 09:54:28 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// Create web server
+// 1. Create web server
+// 2. Read file
+// 3. Write file
+// 4. Redirect
+// 5. Get data from query string
+// 6. Get data from post
+// 7. Get data from cookie
+// 8. Create session
+// 9. Add data to session
+// 10. Get data from session
+// 11. Delete data from session
+// 12. Delete session
+
+// 1. Create web server
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
